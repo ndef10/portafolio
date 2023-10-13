39d55414241292d445930b0ecd140167cf6b4a1c
Marcela <marceladeframchi@gmail.com>
2023-10-12 22:21:27 -0300

    se valida envio de formulario

--- js/index.js
+++ js/index.js
@@ -18,3 +18,57 @@ menuLinks.forEach(menuLinks => {
     })
 })
 
+// contacto
+
+const nombre =document.getElementById('nombre');
+const correo =document.getElementById('correo');
+const asunto =document.getElementById('asunto');
+const mensaje =document.getElementById('mensaje');
+const formulario =document.getElementById('form-contacto');
+const parrafo =document.getElementById('mensaje-exito');
+
+formulario.addEventListener("submit", function (e) {
+    let enviar = false;
+    const regex = (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/); 
+
+    if(nombre.value.trim().length < 3) {
+        nombre.nextElementSibling.classList.add("error");
+        nombre.nextElementSibling.innerHTML = "Ingresa un nombre por favor" 
+        enviar = true 
+    } else {
+        nombre.nextElementSibling.classList.remove("error"); 
+        nombre.nextElementSibling.innerHTML = ""; 
+    }
+
+    if(!regex.test(correo.value)) {
+        correo.nextElementSibling.classList.add("error");
+        correo.nextElementSibling.innerHTML = "El correo ingresado no es válido"
+        enviar = true
+    } else {
+        correo.nextElementSibling.classList.remove("error");
+        correo.nextElementSibling.innerHTML = "";
+    }
+
+    if(mensaje.value.trim().length < 2) {
+        mensaje.nextElementSibling.classList.add("error");
+        mensaje.nextElementSibling.innerHTML = "Incluye un mensaje por favor"
+        enviar = true  
+    } else {
+        mensaje.nextElementSibling.classList.remove("error");
+        mensaje.nextElementSibling.innerHTML = "";
+    }
+
+    if(enviar) {
+        e.preventDefault();
+    } else {
+        parrafo.classList.add("exito")
+        parrafo.innerHTML = "¡Muchas gracias! Mensaje enviado con éxito";
+    }
+});
+
+
+
+
+
+
+
